dc8b29a24db0f71df43d20cc01f4f06b7f578375
Shuaib-786 <124552312+Shuaib-786@users.noreply.github.com>
2024-01-02 15:08:05 +0530

    Add files via upload
    Many type of pattern question solve using C++ .

---
+++ pattern.cpp
@@ -0,0 +1,264 @@
+#include<iostream>
+using namespace std;
+int main(){
+  /*  int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j = 1;
+        while(j<=n){
+            cout<<"*";
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }*/
+    /*
+    int n;
+    cin>>n;
+    int i =1;
+
+    while(i<=n){
+         int j=1;
+         while(j<=n){
+            cout<<i;
+            j=j+1;
+
+         }
+         cout<<endl;
+         i=i+1;
+    }*/
+/*
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<j;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+
+    }
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<n-j+1;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+
+    }
+    int n;
+    cin>>n;
+
+    int i=1;
+    int count =1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<count<<" ";
+            count=count+1;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+    int n;
+    cin>>n;
+     int i =1;
+     
+     while(i<=n){
+         int j=1;
+         while(j<=i){
+            cout<<"*";
+            j=j+1;
+         }
+         cout<<endl;
+         i=i+1;
+     }*
+     int n;
+     cin>>n;
+      int i =1;
+      while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<<i;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+      }
+      int n;
+      cin>>n;
+      int i=1;
+      int count =1;
+      while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<< count;
+            count=count+1;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+      }
+      int n;
+      cin>>n;
+
+      int i=1;
+      while(i<=n){
+        int j=1;
+        int value=i;
+        while(j<=i){
+            cout<<value;
+            value=value+1;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+      }
+      int n;
+      cin>>n;
+       int i=1;
+
+       while(i<=n){
+        int j =1;
+        while(j<=i){
+            cout<<i-j+1;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+       }
+       int n;
+       cin>>n;
+        int i=1;
+        while(i<=n){
+            int j=1;
+            while(j<=n){
+                char ch='a'+i-1;
+                cout<<ch;
+                j=j+1;
+            }
+            cout<<endl;
+            i=i+1;
+        }
+        int n;
+        cin>>n;
+        int i=1;
+          while(i<=n){
+            int j =1;
+            while(j<=n){
+                char ch= 'A'+j-1;
+                cout<<ch;
+                j=j+1;
+            }
+            cout<<endl;
+            i=i+1;
+          }
+      int n;
+      cin>>n;
+      int i=1;
+      while(i<=n){
+        int j =1;
+        while(j<=n){
+            char ch='A'+i+j-2;
+            cout<<ch;
+            j=j+1;
+        }cout<<endl;
+        i=i+1;
+      }
+          
+         int n;
+         cin>>n;
+         int i=1;
+         while(i<=n){
+            int j=1;
+            while(j<=i){
+                char ch=('A'+i-1);
+                cout<<ch;
+                j=j+1;
+            }cout<<endl;
+            i=i+1;
+         } 'A'+row+col-2
+
+         int n;
+         cin>>n;
+        int i=1;
+        while(i<=n){
+            int col=1;
+            char start='A'+n-i;
+            while(col<=i){
+                cout<<start;
+                start=start+1;
+                col=col+1;
+            }
+            cout<<endl;
+            i=i+1;
+        }
+     question  *
+              **
+             ***
+            ****
+
+        int n;
+        cin>>n;
+        int i=1;
+        while(i<=n){
+            //space print
+            int space=n-i;
+            while(space){
+                cout<<" ";
+            space=space-1;
+            }
+         // star print
+            int j=1;
+            while(j<=i){
+                cout<<"*";
+                j=j+1;
+            }
+            cout<<endl;
+            i=i+1;
+         
+
+         }
+Question    
+   1
+  121
+ 12321
+1234321
+        int n;
+        cin>>n;
+        int i=1;
+        while(i<=n){
+            //print space
+            int space=n-i;
+            while(space){
+                cout<<" ";
+                space=space-1;
+            }
+
+            //print 1st triangle
+            int j=1;
+            while(j<=i){
+                cout<<j;
+                j=j+1;
+            }
+            // second triangle
+            int start=i-1;
+            while(start){
+                cout<<start;
+                start=start-1;
+            }
+            cout<<endl;
+            i=i+1;
+
+        }*/
+}
